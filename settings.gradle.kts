ffd3c90d60d8dadbf0c851d8bee6a8f37f9a875b
2756826865 <2756826865@qq.com>
2026-08-04 23:38:27 +0800

    ci: 优先使用官方 Maven 仓库

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,23 +7,23 @@ pluginManagement {
         }
     }
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
         gradlePluginPortal()
         google()
         mavenCentral()
+        maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
     }
 }
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
-        maven { setUrl("https://maven.aliyun.com/repository/google") }
-        maven { setUrl("https://maven.aliyun.com/repository/central") }
-        maven { setUrl("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         maven { setUrl("https://www.jitpack.io") }
+        maven { setUrl("https://maven.aliyun.com/repository/google") }
+        maven { setUrl("https://maven.aliyun.com/repository/central") }
+        maven { setUrl("https://maven.aliyun.com/repository/public") }
         mavenLocal()
     }
 }
