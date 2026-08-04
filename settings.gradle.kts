0a37516582aee69c0273efdca16b9499d6691994
2756826865 <2756826865@qq.com>
2026-08-04 23:29:29 +0800

    ci: 稳定解析 KSP 插件

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,11 @@
 pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.google.devtools.ksp") {
+                useModule("com.google.devtools.ksp:symbol-processing-gradle-plugin:${requested.version}")
+            }
+        }
+    }
     repositories {
         maven { setUrl("https://maven.aliyun.com/repository/gradle-plugin") }
         maven { setUrl("https://maven.aliyun.com/repository/google") }
