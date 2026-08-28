2544e02c70c9800bb623bda0825a211f7c338be4
2756826865 <2756826865@qq.com>
2026-08-28 14:02:40 +0800

    v1.1.2: 完善消息模板管理与 SMS Gateway 开发版功能，优化 UI 品牌色还原

---
+++ app/src/main/kotlin/org/fossify/messages/activities/MessageTemplateActivity.kt
@@ -0,0 +1,20 @@
+﻿package org.fossify.messages.activities
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import org.fossify.messages.ui.compose.rules.RuleStudioScreen
+import org.fossify.messages.ui.compose.theme.GatewayTheme
+
+class MessageTemplateActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GatewayTheme {
+                RuleStudioScreen(
+                    onBack = { finish() }
+                )
+            }
+        }
+    }
+}
